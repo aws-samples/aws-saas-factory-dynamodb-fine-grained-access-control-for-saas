d53dc06bb71df61808384ea35ae4874f9f83b7ed
Dhammika <sriyana@amazon.com>
2023-10-03 02:44:14 +0800

    Updated setup.sh file to CDK Bootstrap with new regions

--- setup.sh
+++ setup.sh
@@ -1,11 +1,15 @@
-echo "Current Region is : $AWS_REGION"
-printf "Enter the AWS region name you want to deploy the solution below (Just press Enter if it's for us-east-1) : "
+echo "\nCleaning the local workspace and building the solution..."
+rm -rf cdk.out/
+rm -rf node_modules/
+rm package-lock.json
+npm install
+
+echo "Your current Region is : $AWS_REGION"
+printf "Enter the AWS region name you want to deploy the solution (Just press Enter if it's for \"$AWS_REGION\") : "
 read -r region
 
-if [ -z "$region" ]
+if [ -n "$region" ]
 then
-  export AWS_REGION=us-east-1
-else
   export AWS_REGION=$region
 fi
 
@@ -14,12 +18,6 @@ echo "Region to deploy the solution : $AWS_REGION"
 printf "Bootstrapping the environment to run CDK app..."
 ACC_ID=`echo \`aws sts get-caller-identity --query "Account" --output text\``
 cdk bootstrap "aws://$ACC_ID/$AWS_REGION"
-
-printf "\nCleaning the local workspace and building the solution..."
-rm -rf cdk.out/
-rm -rf node_modules/
-rm package-lock.json
-npm install
 
 # Build the Cloudformation scripts and and deploy the AWS components on the AWS Account
 cdk ls; cdk synth; cdk deploy;
